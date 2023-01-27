eabf16aea23d2bb5eb19deb27b2a4bca7ab45b75
unclejames2610 <udealaebuka@gmail.com>
2023-01-27 11:32:30 +0100

    Context

--- src/App.js
+++ src/App.js
@@ -51,12 +51,24 @@ function App() {
           >
             <Sidebar />
           </div> */}
-          {activeMenu ? (
+          {/* if (activeMenu)
+          {
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
-          ) : (
+          }
+          else
+          {
             <div className="w-0  dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          } */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg ">
               <Sidebar />
             </div>
           )}
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,8 +11,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setisClicked] = useState(initialState);
+
+  const handleClick = (clicked) => {
+    setisClicked({ ...initialState, [clicked]: true });
+  };
+
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setisClicked,
+        handleClick,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
