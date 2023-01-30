bd692c75c47aef5e514569a180107fe9a289ce0a
unclejames2610 <udealaebuka@gmail.com>
2023-01-30 18:13:19 +0100

    Homepage Monday

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -60,11 +60,34 @@ const Ecommerce = () => {
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p>
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
