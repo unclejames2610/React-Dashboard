67b71a6606cb013faf7a2e9928545103188eee9e
unclejames2610 <udealaebuka@gmail.com>
2023-02-02 17:57:48 +0100

    Customers start

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return (
-    <div>Customers</div>
-  )
-}
+  return <div>Customers</div>;
+};
 
-export default Customers
+export default Customers;
