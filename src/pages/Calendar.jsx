a5bb575cf1538c7b6a2f3dab799dd2db20bd51c5
unclejames2610 <udealaebuka@gmail.com>
2023-02-06 10:24:46 +0100

    Customers end

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+  return <div>Calendar</div>;
+};
 
-export default Calendar
+export default Calendar;
