3c2d0099fbd84ec82bb696974ee891be8744e958
unclejames2610 <udealaebuka@gmail.com>
2023-02-08 17:39:39 +0100

    LinePage

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
-  return (
-    <div>LineChart</div>
-  )
-}
+  return <div>LineChart</div>;
+};
 
-export default LineChart
+export default LineChart;
