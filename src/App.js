7d6ec519dd5656996c5f0a1d1141df52da46825c
unclejames2610 <udealaebuka@gmail.com>
2023-02-09 09:09:38 +0100

    Sidebar fixed

--- src/App.js
+++ src/App.js
@@ -54,25 +54,7 @@ function App() {
               </button>
             </TooltipComponent>
           </div>
-          {/* <div
-            className={`fixed sidebar dark:bg-secondary-dark-bg bg-white ${
-              activeMenu ? "w-72 block" : "w-0 hidden"
-            }`}
-          >
-            <Sidebar />
-          </div> */}
-          {/* if (activeMenu)
-          {
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
-            </div>
-          }
-          else
-          {
-            <div className="w-0  dark:bg-secondary-dark-bg">
-              <Sidebar />
-            </div>
-          } */}
+
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
@@ -83,9 +65,11 @@ function App() {
             </div>
           )}
           <div
-            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72" : "flex-2"
-            }`}
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
