458eeb4063fa4868c0eed47cb29c8711cf12e509
unclejames2610 <udealaebuka@gmail.com>
2023-02-09 13:03:18 +0100

    Pie Financial ColorMap

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { pieChartData } from "../../data/dummy";
+import { Header, PieChart } from "../../components";
 
 const Pie = () => {
   return (
-    <div>Pie</div>
-  )
-}
+    <div
+      className="m-4 md:m-10 mt-24 p-10 
+    bg-white dark:bg-secondary-dark-bg rounded-3xl"
+    >
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <div className="w-full">
+        <PieChart
+          id="chart-pie"
+          data={pieChartData}
+          legendVisibility
+          height="full"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Pie
+export default Pie;
--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject,
+  AccumulationLegend,
+  AccumulationDataLabel,
+  AccumulationTooltip,
+  PyramidSeries,
+  AccumulationSelection,
+} from "@syncfusion/ej2-react-charts";
+
+import { PyramidData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Pyramid = () => {
-  return (
-    <div>Pyramid</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Pyramid
+  return (
+    <div
+      className="m-4 md:m-10 mt-24 p-10 
+    bg-white dark:bg-secondary-dark-bg rounded-3xl"
+    >
+      <Header category="Financial" title="Apple Historical" />
+      <div className="w-full">
+        <AccumulationChartComponent>
+          <Inject />
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective />
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Pyramid;
